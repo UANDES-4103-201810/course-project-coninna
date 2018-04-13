2b32150b6f5618649101edcafe257cc36275f5c0
ConiMarchant <cbmarchant@miuandes.cl>
2018-04-13 14:44:13 -0300

    Fix homepage

--- Coninna/config/routes.rb
+++ Coninna/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :promises
   resources :projects
 
-  get 'coninna' => 'application#index'
+  root  'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
