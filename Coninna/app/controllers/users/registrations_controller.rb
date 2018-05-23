a166f59fe1d8790c04328d6352aac16ab0e3ec07
CaroAssadi <csassadi@miuandes.cl>
2018-05-23 01:22:56 -0400

    Worked on admin views

--- Coninna/app/controllers/users/registrations_controller.rb
+++ Coninna/app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
-    params.require(:user).permit(:name, :user_name, :phone,  :email, :password, :dob, :credit_card_info, :address, :country, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :user_name, :admin, :phone,  :email, :password, :dob, :credit_card_info, :address, :country, :password_confirmation, :current_password)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :user_name, :phone, :email, :password, :dob, :credit_card_info, :address, :country, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :user_name, :admin, :phone, :email, :password, :dob, :credit_card_info, :address, :country, :password_confirmation, :current_password)
   end
 end
