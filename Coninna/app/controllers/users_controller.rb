38fdaaf2fd6ae57021be50ff3d9c2644a73eb8f3
CaroAssadi <csassadi@miuandes.cl>
2018-05-25 01:42:50 -0400

    Fixed admin edit user

--- Coninna/app/controllers/users_controller.rb
+++ Coninna/app/controllers/users_controller.rb
@@ -76,6 +76,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:id, :name, :user_name, :admin)
+      params.require(:user).permit(:id, :name, :user_name, :admin, :phone, :email, :dob, :credit_card_info, :country, :password,:address)
     end
 end
