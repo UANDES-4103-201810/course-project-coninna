bd3a69d8bfa0c2a85b6849a01b208b1a6f700b24
CaroAssadi <csassadi@miuandes.cl>
2018-05-25 00:53:05 -0400

    'show' outstanding in index

--- Coninna/app/controllers/application_controller.rb
+++ Coninna/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
-    @projects = Project.all.limit(1)[0]
+    @projects = Project.all
   end
 
 end
