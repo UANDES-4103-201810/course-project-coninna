e2c5bcbad79501edaf92d8648817908b46cb105e
ConiMarchant <cbmarchant@miuandes.cl>
2018-06-08 17:34:48 -0400

    Method for a monthly mail, and redcarpet gem added

--- Coninna/app/mailers/user_mailer.rb
+++ Coninna/app/mailers/user_mailer.rb
@@ -1,9 +1,20 @@
 class UserMailer < ApplicationMailer
+  require 'date'
+
   default from: 'notifications@example.com'
 
   def welcome_email
     @user = params[:user]
-    @url  = 'http://example.com/login'
+    @url  = 'http://localhost:3000/users/sign_in'
     mail(to: @user.email, subject: 'Welcome the Coninna Crowdfunding Family')
   end
+
+  def run
+     if DateTime.now.day == 1
+      User.find_each do |user|
+        UserMailer.with(user: user).fundflow_email.deliver_now
+      end
+     end
+  end
+
 end
