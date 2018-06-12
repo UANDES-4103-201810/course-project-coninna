71182fcde37767285cb81f23c67edf380ef825b0
CaroAssadi <csassadi@miuandes.cl>
2018-06-12 18:10:40 -0400

    some changes

--- Coninna/app/controllers/callbacks_controller.rb
+++ Coninna/app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def facebook
   @user = User.from_omniauth(request.env["omniauth.auth"] )
   sign_in @user
-    redirect_to edit_user_registration_path
+    redirect_to user_path(@user)
 
 
 
